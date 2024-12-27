88aa6340b658c36b8b60b93eca6a3e7b55a7e8ba
ppcyt-2k <27lephuocphilop6@gmail.com>
2024-12-27 18:07:52 +0700

    .lua

---
+++ .lua
@@ -0,0 +1,41 @@
+
+_G.Config = {
+    ["Melee"] = {
+        ["Superhuman"] = true,
+        ["Death Step"] = true,
+        ["Sharkman Karate"] = true,
+        ["Electric Claw"] = true,
+        ["Dragon Talon"] = true,
+        ["Godhuman"] = true
+    },
+    ["Sword"] = {
+        ["Saber"] = true,
+        ["Rengoku"] = true,
+        ["Canvander"] = true,
+        ["Buddy Sword"] = true,
+        ["Yama"] = true,
+        ["Tushita"] = true,
+        ["True Triple Katana"] = true,
+        ["Cursed Dual Katana"] = true
+    },
+    ["Gun"] = {
+        ["Kabucha"] = true,
+        ["Acidum Rifle"] = true,
+        ["Serpent Bow"] = true
+    },
+    ["Mastery"] = {
+        ["Sword"] = true
+    },
+    ["Setting"] = {
+        ["Enabled"] = true,
+        ["Bring Mob [Less Lag]"] = true,
+        ["Hop Server"] = true,
+        ["Reduce Lag"] = true,
+        ["Notifycation Remove"] = true,
+        ["Hop When Player Nearby"] = {
+            ["Enabled"] = true,
+            ["Radius"] = 350
+        }
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TrashLua/BloxFruits/main/KaitunBeta.RiseEvo"))();
